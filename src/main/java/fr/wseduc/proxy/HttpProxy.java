894ede039d743286785bb7c718600dd11eb0c69e
dboissin <damien.boissin@web-education.net>
2018-01-15 22:37:15 +0100

    migration vertx3

--- src/main/java/fr/wseduc/proxy/HttpProxy.java
+++ src/main/java/fr/wseduc/proxy/HttpProxy.java
@@ -16,27 +16,26 @@
 
 package fr.wseduc.proxy;
 
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.VoidHandler;
-import org.vertx.java.core.buffer.Buffer;
-import org.vertx.java.core.http.HttpClient;
-import org.vertx.java.core.http.HttpClientRequest;
-import org.vertx.java.core.http.HttpClientResponse;
-import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.core.json.JsonObject;
-import org.vertx.java.platform.Verticle;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.*;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HttpProxy extends Verticle {
+public class HttpProxy extends AbstractVerticle {
 
+	private static final Logger log = LoggerFactory.getLogger(HttpProxy.class);
 	private final Map<String, HttpClient> proxies = new HashMap<>();
 
 	@Override
-	public void start() {
+	public void start() throws Exception {
 		super.start();
 
 		loadProxies();
@@ -51,7 +50,7 @@ public class HttpProxy extends Verticle {
 					request.response().setStatusCode(404).end();
 				}
 			}
-		}).listen(container.config().getInteger("port", 8000));
+		}).listen(config().getInteger("port", 8000));
 
 	}
 
@@ -76,7 +75,7 @@ public class HttpProxy extends Verticle {
 	}
 
 	private void loadProxies() {
-		for (Object o : container.config().getArray("proxies")) {
+		for (Object o : config().getJsonArray("proxies")) {
 			if (!(o instanceof JsonObject)) {
 				continue;
 			}
@@ -84,15 +83,16 @@ public class HttpProxy extends Verticle {
 			String location = proxyConf.getString("location");
 			try {
 				URI proxyPass = new URI(proxyConf.getString("proxy_pass"));
-				HttpClient proxy = vertx.createHttpClient()
-						.setHost(proxyPass.getHost())
-						.setPort(proxyPass.getPort())
-						.setMaxPoolSize(container.config().getInteger("poolSize", 32))
+				final HttpClientOptions options = new HttpClientOptions()
+						.setDefaultHost(proxyPass.getHost())
+						.setDefaultPort(proxyPass.getPort())
+						.setMaxPoolSize(config().getInteger("poolSize", 32))
 						.setKeepAlive(true)
-						.setConnectTimeout(container.config().getInteger("timeout", 10000));
+						.setConnectTimeout(config().getInteger("timeout", 10000));
+				HttpClient proxy = vertx.createHttpClient(options);
 				proxies.put(location, proxy);
 			} catch (URISyntaxException e) {
-				container.logger().error("Error when load proxy_pass.", e);
+				log.error("Error when load proxy_pass.", e);
 			}
 		}
 	}
@@ -102,41 +102,41 @@ public class HttpProxy extends Verticle {
 		if (uri.endsWith("%2F") || uri.endsWith("%2f")) {
 			uri = uri.substring(0, uri.length() - 3);
 		}
-		if (container.logger().isDebugEnabled()) {
-			container.logger().debug(uri);
-			container.logger().debug(proxy.getHost() + ":" + proxy.getPort());
+		if (log.isDebugEnabled()) {
+			log.debug(uri);
+			// log.debug(proxy.getHost() + ":" + proxy.getPort());
 		}
 		final HttpClientRequest proxyRequest = proxy.request(request.method(), uri,
 				new Handler<HttpClientResponse>() {
 					public void handle(HttpClientResponse cRes) {
 						request.response().setStatusCode(cRes.statusCode());
-						request.response().headers().set(cRes.headers());
+						request.response().headers().addAll(cRes.headers());
 						request.response().headers().remove("Content-Length");
 						request.response().setChunked(true);
-						cRes.dataHandler(new Handler<Buffer>() {
+						cRes.handler(new Handler<Buffer>() {
 							public void handle(Buffer data) {
 								request.response().write(data);
 							}
 						});
-						cRes.endHandler(new VoidHandler() {
-							public void handle() {
+						cRes.endHandler(new Handler<Void>() {
+							public void handle(Void v) {
 								request.response().end();
 							}
 						});
 					}
 				});
-		proxyRequest.headers().set(request.headers());
+		proxyRequest.headers().addAll(request.headers());
 		proxyRequest.putHeader("X-Forwarded-Host", request.headers().get("Host"));
-		proxyRequest.putHeader("X-Forwarded-For", request.remoteAddress().getHostName());
+		proxyRequest.putHeader("X-Forwarded-For", request.remoteAddress().host());
 		request.response().headers().remove("Content-Length");
 		proxyRequest.setChunked(true);
-		request.dataHandler(new Handler<Buffer>() {
+		request.handler(new Handler<Buffer>() {
 			public void handle(Buffer data) {
 				proxyRequest.write(data);
 			}
 		});
-		request.endHandler(new VoidHandler() {
-			public void handle() {
+		request.endHandler(new Handler<Void>() {
+			public void handle(Void v) {
 				proxyRequest.end();
 			}
 		});
